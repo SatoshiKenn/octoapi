d1776aa03ee08aa45e6bbff5e35c997d2227a2b9
Kennette Guevara <92239496+SatoshiKenn@users.noreply.github.com>
2023-06-11 00:44:05 -0400

    api-docs update

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,6 @@ router.use('/', require('./login'));
 router.use('/auth/google', require('./authentication'));
 router.use('/swagger', require('./swagger'));
 router.use('/travelers', require('./travelers'));
+router.use('/users', require('./users'));
 
 module.exports = router;
